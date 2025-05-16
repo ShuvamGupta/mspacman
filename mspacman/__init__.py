435810be32a89132aff3c25ddfda0c20d693d5af
Gupta <gupta46@ad.fz-rossendorf.de>
2025-05-16 22:35:02 +0200

    Initial pip-installable version of mspacman

---
+++ mspacman/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 14 20:36:11 2025
+
+@author: gupta46
+"""
+from .core import *
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 14 20:37:45 2025
+
+@author: gupta46
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mspacman',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy<2.0.0',
+        'scipy',
+        'pandas',
+        'tifffile',
+        'joblib',
+        'anndata',
+        'tqdm',
+        'scikit-image',
+        'pykuwahara',
+        'matplotlib',
+        'napari'
+    ],
+    author='Shuvam Gupta',
+    description='MSPaCMAn: 3D particle and phase analysis',
+)
