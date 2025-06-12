2a0c4e9c231af72d506d7009a356cd4953df71c7
Shuvam Gupta <shuvamgupta56@gmail.com>
2025-06-12 17:11:57 +0000

    Edit setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mspacman',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'numpy<2.0.0',
